08a87286c2646061d6f20a45a9fc90ad2492da73
eltomato <eltomato@be5596bc-d9cf-11de-ac08-77837bae5fd2>
2009-11-25 15:00:10 +0000

    First PreAlpha Version.

---
+++ trunk/OBattleChess/OBattleChess/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace OBattleChess
+{
+    class GameObject
+    {
+        public Model model = null;
+        public Vector3 position = Vector3.Zero;
+        public Vector3 rotation = Vector3.Zero;
+        public float scale = 1.0f;
+
+        public GameObject(string PreSettings)
+        {
+            if (PreSettings == "std")
+            {
+                rotation = new Vector3(MathHelper.ToRadians(-90.0f), 0.0f, 0.0f);
+                scale = 60.0f;
+            }
+        }
+
+    }
+}
---
+++ trunk/OBattleChess/OBattleChess/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OBattleChess")]
+[assembly: AssemblyProduct("OBattleChess")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+
+[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5716e335-e12f-4bb3-85db-b6e02a386a98")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
